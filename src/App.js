759b47c4f6afd5548680b8bb785f8a4104e96ef0
reyeon1209 <reyeon5368@naver.com>
2020-09-09 17:06:14 +0900

    MAKING THE MOVIE APP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   // react는 class component의 render method를 자동으로 실행
@@ -13,22 +14,47 @@ class App extends React.Component{
     movies: []
   };
 
+  getMovies = async () => {
+    const { // data.data.movies
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      );
+    this.setState({ movies, isLoading: false });
+  }
+
   componentDidMount() { // fetch data
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 5000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return (
-    <div>
-      {isLoading ? "Loading..." : "We are ready"}
-    </div>
+    <section className="container">
+      { isLoading ? (
+        <div className="loader">
+            <span className="loader_text">"Loading..."</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </section>
     );
   }
-
 }
 
 export default App;
